db8d00296a792457d960681c65b3aaf8667fbc1d
ethanrmorris <ethan.morris33@gmail.com>
2022-05-26 10:53:50 -0500

    2 files modified

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const playerEl = document.querySelector('#table');
 function generateOptions(options) {
   return Object.entries(options)
     .map(
-      ([id, player]) =>
-        `<tr><td>${id}</td><td>${player.full_name}</td><td>${player.fantasy_positions}</td><td>${player.years_exp}</td><td>${player.team}</td><td>${player.status}</td></tr>`
+      ([player]) =>
+        `<tr><td>${player.id}</td><td>${player.full_name}</td><td>${player.fantasy_positions}</td><td>${player.years_exp}</td><td>${player.team}</td><td>${player.status}</td></tr>`
     )
     .join('');
 }
